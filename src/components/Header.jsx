b957e0c98b9a9fe67d94207db10b776152d32f24
kolyalak <kolyalak25@yandex.ru>
2025-04-09 21:56:32 +0300

    Update v6(added _redirect to public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,33 +2,36 @@ import React from 'react';
 import '../styles/Header.css'
 import Topbar from './Topbar'
 import logoImage from "../assets/logo.jpg"; // Фото процедури
+import { Link } from 'react-router-dom';
+
 
 
 const Header = () => {
-    window.onscroll = function() {
-        const navbar = document.querySelector('.navbar');
-        const navbarArea = document.querySelector('.navbar-area');      
-        if (window.scrollY > navbarArea.offsetHeight) {
-          navbar.classList.add('sticky');
-        } else {
-          navbar.classList.remove('sticky');
-        }
-      };
-      
-    return (
+  window.onscroll = function () {
+    const navbar = document.querySelector('.navbar');
+    const navbarArea = document.querySelector('.navbar-area');
+    if (window.scrollY > navbarArea.offsetHeight) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
+  };
 
-        <header className="navbar-area">
-            <Topbar/>
-            <div className="navbar">
-                <div className="logo"><img src={logoImage} alt="" /></div>
-                <nav className="links">
-                    <a href="/">Головна </a>
-                    <a href="/service">Послуги </a>
-                    <a href="/contact">Контакти </a>
-                </nav>
-            </div>
-        </header>
-    );
+  return (
+
+    <header className="navbar-area">
+      <Topbar />
+      <div className="navbar">
+        <div className="logo"><img src={logoImage} alt="" /></div>
+        <nav className="links">
+          <Link to="/">Головна</Link>
+          <Link to="/service">Послуги</Link>
+          <Link to="/contact">Контакти</Link>
+        </nav>
+
+      </div>
+    </header>
+  );
 };
 
 export default Header;
